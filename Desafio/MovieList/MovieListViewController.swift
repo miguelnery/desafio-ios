b9f03ba6fad822100216af038764b622be51ba4a
miguelnery <nery.mik@gmail.com>
2021-02-15 17:48:30 -0300

    add basic MovieList presenter + embed presenter on MovieListViewController

--- Desafio/MovieList/MovieListViewController.swift
+++ Desafio/MovieList/MovieListViewController.swift
@@ -4,7 +4,18 @@ import SnapKit
 
 class MovieListViewController: UIViewController {
     private let movieListView = MovieListCollectionView(frame: .zero)
-    private let dataSource = DefaultCollectionViewDataSource<MovieListCollectionCell, Movie>()
+    private let dataSource = DefaultCollectionViewDataSource<MovieListCollectionCell, MovieViewModel>()
+    private let presenter: MovieListPresenterType
+
+    init(presenter: MovieListPresenterType = MovieListPresenter()) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func loadView() {
         view = movieListView
@@ -12,9 +23,10 @@ class MovieListViewController: UIViewController {
 
     override func viewDidLoad() {
         dataSource.attach(collection: movieListView)
-        dataSource.items = [Movie(),
-                            Movie(),
-                            Movie()]
-        movieListView.reloadData()
+        presenter.getPopularMovies { [dataSource] result in
+            if case .success(let viewModels) = result {
+                dataSource.items = viewModels
+            } else { print ("🔥lascou") }
+        }
     }
 }
